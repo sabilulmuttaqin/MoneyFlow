f71beb87874da33dcdb4ac18d0454ed69a84f4a3
youlesejaa <juvendareza630@gmail.com>
2025-11-18 22:47:32 +0700

    pembuatan model user dan migrasi table user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,35 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // Kolom yang dapat diisi mass-assignment
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    // Kolom yang perlu disembunyikan saat serialisasi (seperti ketika mengonversi ke array atau JSON)
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    // Pengaturan casting untuk kolom tertentu
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    // Metode untuk menangani password yang terenkripsi
+    public function setPasswordAttribute($value)
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        $this->attributes['password'] = bcrypt($value); // Mengenkripsi password
     }
 }
