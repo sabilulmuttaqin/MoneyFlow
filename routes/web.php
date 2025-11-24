6eb7d4483001ae53cda765b8faa7f97f624e8f14
fawwaz254 <fawwaznafi562@gmail.com>
2025-11-24 20:13:25 +0700

    Update login UI and configuration. add env example

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/login');
+});
+
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 
