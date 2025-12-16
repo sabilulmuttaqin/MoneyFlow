3f42572c8cebd6b46c697161cef34bc2b155e894
fawwaz254 <fawwaznafi562@gmail.com>
2025-12-16 21:09:49 +0700

    feat: logic interaksi halaman detail tabungan

---
+++ public/js/tabungan-detail.js
@@ -0,0 +1,11 @@
+function openSetoranModal() {
+    document.getElementById('modalSetoran').style.display = 'flex';
+}
+
+function closeSetoranModal() {
+    document.getElementById('modalSetoran').style.display = 'none';
+}
+
+function closeSuccessModal() {
+    document.getElementById('successModal').style.display = 'none';
+}
